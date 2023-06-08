8fe3c6f372e03512cd6edb2e03966f542d1fcfcb
robert willcox <robducks@gmail.com>
2023-06-08 21:46:20 +0100

    layout updates

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,13 +6,14 @@ class UsersController < ApplicationController
   def show
     start_date = params.fetch(:start_date, Date.today).to_date
     @trips = Trip.where(date: start_date.beginning_of_month.beginning_of_week..start_date.end_of_month.end_of_week)
-    @user = current_user
+    @user = User.find(params[:id])
   end
 
   def my_profile
     start_date = params.fetch(:start_date, Date.today).to_date
     @trips = Trip.where(date: start_date.beginning_of_month.beginning_of_week..start_date.end_of_month.end_of_week)
     @user = current_user
+    # @user = User.find(params[:id])
     # @friend = @user.friendships_as_asker.receiver.username
   end
 
