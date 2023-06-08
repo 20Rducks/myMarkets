4a3824bb18abff0e73c90c6614589ef0a09740a4
robert willcox <robducks@gmail.com>
2023-06-08 03:21:14 +0100

    friendship

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,8 @@ class UsersController < ApplicationController
   end
 
   def my_profile
+    start_date = params.fetch(:start_date, Date.today).to_date
+    @trips = Trip.where(date: start_date.beginning_of_month.beginning_of_week..start_date.end_of_month.end_of_week)
     @user = current_user
   end
 end
---
+++ db/migrate/20230608014949_create_friendships.rb
@@ -0,0 +1,9 @@
+class CreateFriendships < ActiveRecord::Migration[7.0]
+  def change
+    create_table :friendships do |t|
+      t.references :asker, null: false, foreign_key: { to_table: :users }
+      t.references :receiver, null: false, foreign_key: { to_table: :users }
+      t.timestamps
+    end
+  end
+end
